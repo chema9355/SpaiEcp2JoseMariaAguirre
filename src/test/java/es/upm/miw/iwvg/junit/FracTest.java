516a0e81812ea3cee10067bdbed9b097e39e7f16
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-10-04 10:40:53 +0200

    Creado test isEquivalent

--- src/test/java/es/upm/miw/iwvg/junit/FracTest.java
+++ src/test/java/es/upm/miw/iwvg/junit/FracTest.java
@@ -4,33 +4,36 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
+
 public class FracTest {
-	private Fraction fraction;
-	
-	@Before
-	public void before() {
-        fraction = new Fraction(1,2);
+    private Fraction fraction;
+
+    @Before
+    public void before() {
+        fraction = new Fraction(1, 2);
     }
 
-	
-	 @Test
-	 public void testFractionIntInt() {
-	      assertEquals(1, fraction.getNumerator());
-	      assertEquals(2, fraction.getDenominator());
-	    }
-	
-	 @Test
-	 public void testFraction() {
-		  fraction = new Fraction(1,1);
-	      assertEquals(1, fraction.getNumerator());
-	      assertEquals(1, fraction.getDenominator());
-	    }
-	
-	 
-	 @Test
-	 public void testDecimal() {
-	      assertEquals(0.5, fraction.decimal(),0.00001);
-	    }
-	
+    @Test
+    public void testFractionIntInt() {
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(2, fraction.getDenominator());
+    }
+
+    @Test
+    public void testFraction() {
+        fraction = new Fraction(1, 1);
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
+    public void testDecimal() {
+        assertEquals(0.5, fraction.decimal(), 0.00001);
+    }
+
+    @Test
+    public void testIsEquivalent() {
+        assertEquals(true, fraction.isEquivalent(new Fraction(2, 4)));
+    }
 
 }
